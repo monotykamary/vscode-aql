cf56977ef8f23817b7d229a3b405c0f29d6a519f
Tom Nguyen <tom81094@gmail.com>
2021-11-05 17:25:32 +0700

    chore: update data to v3.8.2

--- from-docker.sh
+++ from-docker.sh
@@ -1,10 +1,10 @@
 #/bin/sh
-sudo docker run -e ARANGO_NO_AUTH=1 -p 8529:8529 -d --name arangodb arangodb:3.8.1
-sleep 10
+sudo docker run -e ARANGO_NO_AUTH=2 -p 8529:8529 -d --name arangodb arangodb:3.8.2
+sleep 20
 
 curl http://localhost:8529/_db/_system/_api/aql-builtin | json_pp > data/aql-functions.json
 node ./generateData.js
 
 sudo docker stop arangodb
-sudo docker container prune -f
+sudo docker container rm arangodb
 sudo docker volume prune -f
