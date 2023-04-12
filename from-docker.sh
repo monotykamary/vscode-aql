de054dc01594dde300dd514bf6b97b24c9652783
Tom X Nguyen <tom81094@gmail.com>
2023-04-12 09:50:17 +0700

    chore: bump to 3.10.5

--- from-docker.sh
+++ from-docker.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-sudo docker run -e ARANGO_NO_AUTH=2 -p 8529:8529 -d --name arangodb arangodb:3.9.1
+sudo docker run -e ARANGO_NO_AUTH=2 -p 8529:8529 -d --name arangodb arangodb:3.10.5
 sleep 20
 
 curl http://localhost:8529/_db/_system/_api/aql-builtin | json_pp > data/aql-functions.json
