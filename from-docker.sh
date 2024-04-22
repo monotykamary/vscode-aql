8924174235f0e56fd23dff744defe55a379a1a22
Tom X Nguyen <tom81094@gmail.com>
2024-04-22 15:09:47 +0700

    chore: update arangodb functions to v3.12.0 and move towards devbox

--- from-docker.sh
+++ from-docker.sh
@@ -1,10 +1,10 @@
 #/bin/sh
-sudo docker run -e ARANGO_NO_AUTH=2 -p 8529:8529 -d --name arangodb arangodb:3.11.6
+podman run -e ARANGO_NO_AUTH=2 -p 8529:8529 -d --name arangodb arangodb:3.12.0
 sleep 20
 
 curl http://localhost:8529/_db/_system/_api/aql-builtin | json_pp > data/aql-functions.json
 node ./generateData.js
 
-sudo docker stop arangodb
-sudo docker container rm arangodb
-sudo docker volume prune -f
+podman stop arangodb
+podman container rm arangodb
+podman volume prune -f
