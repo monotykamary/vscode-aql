1fed360c19a422b4c2b9c9682e6dd2b2b6c3bd27
Tom X Nguyen <tom81094@gmail.com>
2024-05-15 09:09:24 +0700

    chore: revert to docker

--- from-docker.sh
+++ from-docker.sh
@@ -1,10 +1,10 @@
 #/bin/sh
-podman run -e ARANGO_NO_AUTH=2 -p 8529:8529 -d --name arangodb arangodb:3.12.0
+docker run -e ARANGO_NO_AUTH=2 -p 8529:8529 -d --name arangodb arangodb:3.12.0.2
 sleep 20
 
 curl http://localhost:8529/_db/_system/_api/aql-builtin | json_pp > data/aql-functions.json
 node ./generateData.js
 
-podman stop arangodb
-podman container rm arangodb
-podman volume prune -f
+docker stop arangodb
+docker container rm arangodb
+docker volume prune -f
