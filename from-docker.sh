38fab92f3a720ac8315cbe4f4a3800536f3612bc
Tom X Nguyen <tom81094@gmail.com>
2025-04-21 20:41:54 +0700

    chore: update keywords and functions for ArangoDB v3.12.4

--- from-docker.sh
+++ from-docker.sh
@@ -1,6 +1,6 @@
 #/bin/sh
-docker run -e ARANGO_NO_AUTH=2 -p 8529:8529 -d --name arangodb arangodb:3.12.1
-sleep 20
+docker run -e ARANGO_NO_AUTH=2 -p 8529:8529 -d --name arangodb arangodb:3.12.4
+sleep 60
 
 curl http://localhost:8529/_db/_system/_api/aql-builtin | json_pp > data/aql-functions.json
 node ./generateData.js
