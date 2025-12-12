b4b8eb04d43ace7069fb779085422ac3080e047f
Tom X Nguyen <tom81094@gmail.com>
2025-12-13 00:43:35 +0700

    chore: update gitignore to exclude build artifacts
    - Remove compiled output (client/out, server/out) from version control
    - Exclude tsconfig.tsbuildinfo files
    - Exclude lock files (bun.lock, package-lock.json)

--- server/out/docs/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { FunctionDocumentation, functionDocs, FunctionSignature, ParameterInfo } from './functions';
-export { KeywordInfo, KEYWORDS, getKeywordInfo, getAllKeywordNames, getAllKeywords, getKeywordsByCategory, getKeywordMarkdown } from './keywords';
--- server/out/parser/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { AqlLexer, allTokens } from './lexer';
-export * from './lexer';
-export { AqlParser, aqlParser } from './parser';
-export * from './types';
-export { AqlAnalyzer, analyzer, CompletionContext } from './analyzer';
